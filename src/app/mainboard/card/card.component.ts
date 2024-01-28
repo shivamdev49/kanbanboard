2f86e201caa640570d453ab7f94b2c9fed800799
shivamdev49 <shivamcp49@gmail.com>
2024-01-28 10:19:46 +0530

    some changes

--- src/app/mainboard/card/card.component.ts
+++ src/app/mainboard/card/card.component.ts
@@ -9,4 +9,5 @@ import { Component, Input } from '@angular/core';
 export class CardComponent {
 
   @Input() cards: any;
+
 }
--- src/app/mainboard/mainboard.component.ts
+++ src/app/mainboard/mainboard.component.ts
@@ -10,10 +10,26 @@ import { Card } from 'src/model/card';
 export class MainboardComponent {
 
   tasks:Card[]=[];
+  task2:Card[]=[]
   constructor(private service : KanbanService){
      this.tasks = this.service.getTask();
   }
  
 
-  
+  onDragStarted(event: any): void {
+    console.log('Drag started:', event);
+  }
+
+  onDragEnded(event: any): void {
+    console.log('Drag ended:', event);
+  }
+//rohit made this
+/*
+  onDrop(event: CdkDragDrop<t[]>): void {
+    moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
+  }
+  */
 }
+
+  
+
